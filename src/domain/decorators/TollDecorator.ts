1ba843531f297072d3aab436bbc3b3f7ec25f8e2
jManuel0 <juan.ordonezar@campusucc.edu.co>
2025-11-07 15:39:09 -0500

    Correcion de TollDecorator

--- src/domain/decorators/TollDecorator.ts
+++ src/domain/decorators/TollDecorator.ts
@@ -1,9 +1,13 @@
 import { TripDecorator } from "./TripDecorator";
 import PricingConfig from "../PricingConfig";
+import type { Trip } from "../Trip";
 
 export class TollDecorator extends TripDecorator {
-  constructor(wrapped: any, private tolls: number) {
+  private readonly tolls: number;
+
+  constructor(wrapped: Trip, tolls: number) {
     super(wrapped);
+    this.tolls = tolls;
   }
 
   calculatePrice(): number {
