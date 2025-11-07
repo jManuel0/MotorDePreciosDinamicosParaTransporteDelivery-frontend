d71e54bca7d20ef4f29bc4b9b4927fab28123d2b
jManuel0 <juan.ordonezar@campusucc.edu.co>
2025-11-07 16:20:43 -0500

    correcion Peak

--- src/domain/decorators/PeakHourDecorator.ts
+++ src/domain/decorators/PeakHourDecorator.ts
@@ -1,7 +1,12 @@
 import { TripDecorator } from "./TripDecorator";
+import type { Trip } from "../Trip";
 import PricingConfig from "../PricingConfig";
 
 export class PeakHourDecorator extends TripDecorator {
+  constructor(wrapped: Trip) {
+    super(wrapped);
+  }
+
   calculatePrice(): number {
     const base = this.wrapped.calculatePrice();
     return base * PricingConfig.getInstance().getPeakHourMultiplier();
